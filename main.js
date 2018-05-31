2cb47ba003a7da3ec73f74bae6a063c2f6316dde
Gabe Nunez <gnunez@prosofteng.com>
2018-05-31 13:59:13 -0700

    Set bot command prefix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const prefix = "$";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
