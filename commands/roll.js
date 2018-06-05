d4ccb25fa5da2dfd3fa10465a070beb96e642793
Gabe Nuñez <gabe@gabenunez.com>
2018-06-05 14:13:43 -0700

    Feature: Add, Multiply, and Subtract from your total rolled dice

--- commands/roll.js
+++ commands/roll.js
@@ -4,9 +4,8 @@ module.exports = {
     execute(msg, args) {
         const messages = require('../modules/messages.js')(msg);
 
-        // TODO: Need to add the ability to "add" or "subtract". i.e: 4d6 +4
-        let [amountOfDice, typeOfDie] = args;
-        let diceRolls = [], diceMessage = "", diceTotal = 0;
+        let [amountOfDice, typeOfDie, operand, opNumber] = args;
+        let diceRolls = [], diceMessage = "", diceTotal = 0, alteredDiceTotal = 0;
 
         // Check if both arguments have a number in them.
         if( !/\d/.test(amountOfDice) || !/\d/.test(typeOfDie) ) {
@@ -29,11 +28,45 @@ module.exports = {
             }
                 
             diceRolls.forEach((num, index) => {
-            diceMessage += `${num} ${index >= amountOfDice - 1 ? '' : ', '}`;
-            diceTotal += num;
+                diceMessage += `${num} ${index >= amountOfDice - 1 ? '' : ', '}`;
+                diceTotal += num;
             });
-    
-            messages.sendChannelMessage(`**:game_die: Dice Rolled (${amountOfDice} d${typeOfDie}):** \n\n\`${diceMessage}\` \n\nTotal: ${diceTotal}`);
+            
+            // Check if any operator should be applied.
+
+            if (operand) {
+                if( !/\d/.test(opNumber) ) {
+                    messages.sendErrorMessage('Looks like your operational number isn\'t actually a number.');
+                    return;
+                } else {
+                    opNumber = Number(opNumber);
+
+                    if (opNumber < 1 || opNumber > 100 ) {
+                        messages.sendErrorMessage('You can\'t have an operational number less than 1 or more than 100 or less than 1.');
+                        return;
+                    }
+                }
+
+                switch(operand) {
+                    case "+":
+                        alteredDiceTotal = diceTotal + opNumber;
+                        break;
+                    case "-":
+                        alteredDiceTotal = diceTotal - opNumber;
+                        break;
+                    case "*":
+                        alteredDiceTotal = diceTotal * opNumber;
+                        break;
+                    case "/": 
+                        messages.sendErrorMessage('Sorry, division (/) is currently not supported.');
+                        return;
+                    default: 
+                        messages.sendErrorMessage('Please enter a usable Math operator (+, -, *).');
+                        return;
+                }
+            }
+
+            messages.sendChannelMessage(`**:game_die: Dice Rolled (${amountOfDice} d${typeOfDie}):** \n\n\`${diceMessage}\` \n\nTotal: ${diceTotal} ${operand ? operand + ' ' + opNumber + ' = **' + alteredDiceTotal + '**' : ""}`);
         }
     },
 };
