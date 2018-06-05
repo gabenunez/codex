c8d6674193286d8b9fdfd3b6785b1d9f8a61d528
Gabe Nuñez <gabe@gabenunez.com>
2018-06-05 13:02:31 -0700

    Restructured dice rollin

--- commands/roll.js
+++ commands/roll.js
@@ -8,30 +8,36 @@ module.exports = {
         let [amountOfDice, typeOfDie] = args;
         let diceRolls = [], diceMessage = "", diceTotal = 0;
 
+        // Check if both arguments have a number in them.
+        if( !/\d/.test(amountOfDice) || !/\d/.test(typeOfDie) ) {
+            messages.sendErrorMessage('An argument doesn\'t have a number!');
+            return;
+        }
+
         amountOfDice = Number(amountOfDice);
         typeOfDie = Number(typeOfDie.replace(/\D/g,''));
 
-        // Error catching for text-only entries, amount of dice rolled, and type of dice.
-        if(amountOfDice === false || typeOfDie == false) {
-          messages.sendErrorMessage('An argument doesn\'t have a number!');
-          return;
+        // Rules and logic of dice rollin'
+
+        if (amountOfDice < 1) {
+            messages.sendErrorMessage('You can\'t roll a die less than 1 times.');
+        } else if (typeOfDie < 2) {
+            messages.sendErrorMessage('You can\'t roll a die with less than 2 sides.');
         } else if (amountOfDice > 100) {
-          messages.sendErrorMessage('You can\'t roll a die more than 100 times.');
-          return;
+            messages.sendErrorMessage('You can\'t roll a die more than 100 times.');
         } else if (typeOfDie > 1000) {
-          messages.sendErrorMessage('You can\'t roll anything higher than a d1000.');
-          return;
+            messages.sendErrorMessage('You can\'t roll anything higher than a d1000.');
+        } else {
+            for (let i = 0; i < amountOfDice ; i++) {
+                diceRolls.push(Math.floor(Math.random() * typeOfDie) + 1);
+            }
+                
+            diceRolls.forEach((num, index) => {
+            diceMessage += `${num} ${index >= amountOfDice - 1 ? '' : ', '}`;
+            diceTotal += num;
+            });
+    
+            messages.sendChannelMessage(`**:game_die: Dice Rolled (${amountOfDice} d${typeOfDie}):** \n\n\`${diceMessage}\` \n\nTotal: ${diceTotal}`);
         }
-
-        for (let i = 0; i < amountOfDice ; i++) {
-          diceRolls.push(Math.floor(Math.random() * typeOfDie) + 1);
-        }
-        
-        diceRolls.forEach((num, index) => {
-          diceMessage += `${num} ${index >= amountOfDice - 1 ? '' : ', '}`;
-          diceTotal += num;
-        });
-
-        messages.sendChannelMessage(`**:game_die: Dice Rolled (${amountOfDice} d${typeOfDie}):** \n\n\`${diceMessage}\` \n\nTotal: ${diceTotal}`);
     },
 };
